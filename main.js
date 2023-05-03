e936c91046c92c3d0b24d5f30e8d145fb1c98bd6
Ahaan-Mandal <122046904+Ahaan-Mandal@users.noreply.github.com>
2023-05-03 13:01:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -12,7 +12,11 @@ poseNet.on('pose', gotPoses);
 
 function draw ()
 {
-    background('#ba8706');     
+    background('#ba8706');   
+    textSize(difference);
+    fill('#dc0e2b');
+    text("Ahaan", 50, 150 );
+    document.getElementById("font_size").innerHTML= "Font size of the text will be " + difference + "px";
 }
 
 function modelLoaded()
@@ -25,5 +29,8 @@ function gotPoses(results)
 if (results.length > 0)
 {
     console.log(results);
+    left_wristX=results[0].pose.leftWrist.x;
+    right_wristX=results[0].pose.rightWrist.x;
+    difference = floor(left_wristX - right_wristX);
 }
 }
